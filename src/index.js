9bf7563787488b5ba113e20c69c48114492e60ff
Alexis Canepa <webdeveloper1@thedolphinco.com>
2022-08-17 20:13:09 -0500

    Creando las imágenes con JavaScript

--- src/index.js
+++ src/index.js
@@ -4,3 +4,41 @@
  **/
 
 console.log('Happy hacking :)')
+const reference_div = document.querySelector("#images");
+
+function randomNumber(){
+    const random_number = Math.floor(Math.random() * 123) + 1;
+
+    return random_number;
+}
+
+function NewImage(){
+    const newimage = document.createElement("img");
+    newimage.width = "320";
+    newimage.classList.add("mx-auto");
+    newimage.src = `https://randomfox.ca/images/${randomNumber()}.jpg`;
+
+    return newimage;
+}
+
+function NewContainer(){
+    const newimagecontainer = document.createElement("div");
+    newimagecontainer.classList.add("p-4");
+
+    return newimagecontainer;
+}
+
+function NewContentCreation(){
+    const newimage = NewImage();
+    const newcontainer = NewContainer();
+
+    newcontainer.append(newimage);
+    reference_div.append(newcontainer);
+}
+
+
+
+document.querySelector("#newImgBtn").addEventListener('click', NewContentCreation);
+
+
+
